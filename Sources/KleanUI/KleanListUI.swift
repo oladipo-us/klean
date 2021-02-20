4c6508e658fbf53dfbb454bbf90e199131d2df52
Grant Oladipo <grant@oladipo.us>
2021-02-20 14:40:51 -0500

    Add KleanListUI

---
+++ Sources/KleanUI/KleanListUI.swift
@@ -0,0 +1,95 @@
+import Foundation
+import UIKit
+
+import KleanFoundation
+import KleanUIModels
+
+public class KleanListUI<ActionType: Hashable, IdentifierType: Hashable, SectionType: Hashable>: KleanUI {
+    
+    // MARK: - Public
+    
+    public typealias DataSourceType = UICollectionViewDiffableDataSource<SectionType, KleanLabelItemUIModel<ActionType, IdentifierType>>
+    public typealias ListCellRegistrationType = UICollectionView.CellRegistration<UICollectionViewListCell, KleanLabelItemUIModel<ActionType, IdentifierType>>
+    
+    public lazy var listDataSource: DataSourceType = {
+        buildListDataSource(
+            list: list,
+            cellRegistration: listCellRegistration)
+    }()
+    
+    // MARK: - Internal
+    
+    required init(translatesAutoresizingMaskIntoConstraints tamic: Bool = true) {
+        super.init(translatesAutoresizingMaskIntoConstraints: tamic)
+
+        list.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        addSubview(list)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatal_klean_notImplemented("init(coder:)")
+    }
+
+    lazy var list: UICollectionView = {
+        buildList(listLayout: listLayout)
+    }()
+    
+    lazy var listCellRegistration: ListCellRegistrationType = {
+        buildListCellRegistration()
+    }()
+    
+    lazy var listLayout: UICollectionViewLayout = {
+        buildListLayout()
+    }()
+    
+    // MARK: - Private
+    
+    private func buildList(
+        listLayout: UICollectionViewLayout
+    ) -> UICollectionView
+    {
+        return UICollectionView(
+            frame: .zero,
+            collectionViewLayout: listLayout)
+    }
+    
+    private func buildListCellRegistration(
+    ) -> ListCellRegistrationType
+    {
+        return ListCellRegistrationType { (cell, indexPath, item) in
+            
+            var content = cell.defaultContentConfiguration()
+            content.text = item.labelString
+            cell.accessories = item.shouldShowDisclosure ? [.disclosureIndicator()] : []
+            cell.contentConfiguration = content
+        }
+    }
+    
+    private func buildListDataSource(
+        list: UICollectionView,
+        cellRegistration: ListCellRegistrationType
+    ) -> DataSourceType
+    {
+        return DataSourceType(
+            collectionView: list)
+        { list, indexPath, identifier -> UICollectionViewCell? in
+            return list.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: identifier)
+        }
+    }
+    
+    private func buildListLayout(
+    ) -> UICollectionViewLayout
+    {
+        return UICollectionViewCompositionalLayout { sectionIndex, layoutEnvironment in
+            
+            let configuration = UICollectionLayoutListConfiguration(
+                appearance: .insetGrouped)
+            
+            let section = NSCollectionLayoutSection.list(
+                using: configuration,
+                layoutEnvironment: layoutEnvironment)
+            
+            return section
+        }
+    }
+}
