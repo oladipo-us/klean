b3748249251c24c3426bb71c019ef283c20cdddc
Grant Oladipo <grant@oladipo.us>
2021-02-21 16:24:27 -0500

    Update label item model with modifiers

--- Sources/KleanUIModels/KleanLabelItemUIModel.swift
+++ Sources/KleanUIModels/KleanLabelItemUIModel.swift
@@ -1,21 +1,65 @@
+import Combine
 import Foundation
 
 public struct KleanLabelItemUIModel<Action: Hashable, Identifier: Hashable>: Hashable {
     
+    public typealias ByIdentifier = [Identifier: Self]
+    public typealias ByIdentifierPublisher = AnyPublisher<ByIdentifier, Never>
+
+    public typealias IdentifierList = [Identifier]
+    public typealias IdentifierListPublisher = AnyPublisher<IdentifierList, Never>
+    public typealias IdentifierPublisher = AnyPublisher<Identifier, Never>
+
+    public typealias List = [Self]
+    public typealias ListPublisher = AnyPublisher<List, Never>
+
+    public typealias Publisher = AnyPublisher<Self, Never>
+
     public init(
         id i: Identifier,
         action a: Action,
         labelString ls: String,
         shouldShowDisclosure ssd: Bool = false)
     {
-        identifier = i
+        id = i
         action = a
         labelString = ls
         shouldShowDisclosure = ssd
     }
     
-    public let identifier: Identifier
-    public let action: Action
+    @available(*, deprecated, renamed: "id")
+    public var identifier: Identifier {
+        return id
+    }
+    
+    public let id: Identifier
+    
     public let labelString: String
     public let shouldShowDisclosure: Bool
+
+    public let action: Action
+}
+
+extension KleanLabelItemUIModel {
+    
+    public final class Factory {
+        
+        public static func constructIdentifierList(
+            list: List
+        ) -> IdentifierList
+        {
+            return list.map { $0.id }
+        }
+        
+        public static func constructByIdentifier(
+            list: List
+        ) -> ByIdentifier
+        {
+            var dict = ByIdentifier()
+            
+            list.forEach { dict[$0.id] = $0 }
+            
+            return dict
+        }
+    }
 }
